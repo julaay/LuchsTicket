b830a54158162b64b4534562806eac5b2ea21a3b
julaay <juliaeigner1@gmail.com>
2018-02-11 21:05:15 +0100

    yooo test

---
+++ src/diplomarbeit/Diplomarbeit.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diplomarbeit;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author July
+ */
+public class Diplomarbeit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        
+    /**    Connection myconnection =null; 
+        
+        
+        Statement mystatement= null; 
+        
+        
+        mystatement=myconnection.createStatement();
+        ResultSet myresultset= null; 
+        
+        
+        try{
+           myconnection=DriverManager.getConnection("jdbc:derby://localhost:1527/termine_dip" , "luchsticket", "luchsticket");
+            
+        }catch (SQLException e){
+            
+            e.printStackTrace();
+            
+        }
+        *     **/
+    }
+
+}
